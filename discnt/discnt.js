c664c9f0b5898f0007ef9de8017570ebaa76e7cd
Yaroslav Dmytriv <yaroslav_dmytriv@epam.com>
2016-04-12 16:58:51 +0300

    change algo to insertion sort & measure file reads & writes as well as algo running time

--- discnt/discnt.js
+++ discnt/discnt.js
@@ -3,6 +3,8 @@
 const sorters = require('./sorters');
 const fs = require('fs');
 
+console.time('Running Time');
+
 const inputFileName = process.argv[2] || 'discnt.in',
   outputFileName = process.argv[3] || 'discnt.out';
 
@@ -11,12 +13,10 @@ fs.readFile(inputFileName, 'utf8', (err, data) => {
   let contents = data.split('\n'),
     input = contents[0].split(' ').map(x => parseInt(x)),
     discnt = parseInt(contents[1]) / 100;
-
-  console.time('Running Time');
   
   let l = input.length,
     discntItemsCount = (l - l % 3) / 3,
-    sorted = sorters.selection(input, discntItemsCount),
+    sorted = sorters.insertion(input),
     discounted = sorted.splice(0, discntItemsCount),
     result = discounted.reduce((acc, sel) => {
       return acc + sel * (1 - discnt);
@@ -25,10 +25,10 @@ fs.readFile(inputFileName, 'utf8', (err, data) => {
   result += sorted.reduce((acc, sel) => acc + sel, 0);
   result = result.toFixed(2);
   
-  console.timeEnd('Running Time');
 
   fs.writeFile(outputFileName, result, (err, data) => {
     if (err) throw err;
     console.log(`Wrote ${result} to ${outputFileName}`);
+    console.timeEnd('Running Time');
   })
 });
