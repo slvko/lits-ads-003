234d5c7e5f7b25fb5ab4289e35480c84ff2c9f96
Yaroslav Dmytriv <yaroslav_dmytriv@epam.com>
2016-05-28 12:14:17 +0300

    all zeroes case

--- lngpok/lngpok.js
+++ lngpok/lngpok.js
@@ -1,7 +1,21 @@
 'use strict';
-// console.time('Took: ');
+console.time('Took: ');
 const fs = require('fs');
 // const sorters = require('./sorters');
+
+Promise.all(
+  Array(15).fill().map((v, i) => {
+    ++i;
+    let pad = i < 10 ? '0' : '';
+    return new Promise((res, rej) => {
+      fs.readFile(`./answers/${pad}${i}.out`, 'utf8', (err, data) => {
+        res(parseInt(data));
+      });
+    });
+  })
+).then(answers => {
+  answers.map((ans, i) => console.log(`${++i}:\t${ans}`));
+});
 
 const taskName = 'lngpok';
 const inputFileName = process.argv[2] || taskName + '.in',
@@ -27,22 +41,26 @@ fs.readFile(inputFileName, 'utf8', (err, data) => {
   }
   // cardsAry = sorters.merge(cardsAry);
   let longestSeqLength = 1;
-  cardsAry.forEach(card => {
-    if (cardsMap[card - 1]) {
-      // do not count for second-in-row number
-      return null;
-    }
-    let zeroFills = zeroesCnt;
-    let currentSeqLength = 1;
-    while (cardsMap[card + currentSeqLength] || zeroFills--) {
-      ++currentSeqLength;
-    }
-    longestSeqLength = Math.max(longestSeqLength, currentSeqLength);
-  });
+  if (!cardsAry.length && zeroesCnt) {
+    longestSeqLength = zeroesCnt;
+  } else {
+    cardsAry.forEach(card => {
+      if (cardsMap[card - 1]) {
+        // do not count for second-in-row number
+        return null;
+      }
+      let zeroFills = zeroesCnt;
+      let currentSeqLength = 1;
+      while (cardsMap[card + currentSeqLength] || zeroFills--) {
+        ++currentSeqLength;
+      }
+      longestSeqLength = Math.max(longestSeqLength, currentSeqLength);
+    });
+  }
 
   fs.writeFile(outputFileName, longestSeqLength, (err, data) => {
     if (err) throw err;
-    // console.log(`Longest: ${longestSeqLength}, Zeroes: ${zeroesCnt}`)
-    // console.timeEnd('Took: ');
+    console.log(`Longest: ${longestSeqLength}, Zeroes: ${zeroesCnt}`)
+    console.timeEnd('Took: ');
   });
 });
