2ba4157ab7211ca0760407aff2608c4f62d7ba89
Yaroslav Dmytriv <yaroslav_dmytriv@epam.com>
2016-05-30 03:30:32 +0300

    at least #5 passes now :)

--- sigkey/sigkey.js
+++ sigkey/sigkey.js
@@ -6,30 +6,42 @@ const taskName = 'sigkey';
 const inputFileName = process.argv[2] || taskName + '.in',
   outputFileName = process.argv[3] || taskName + '.out';
 
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const alphabetLength = alphabet.length;
+
 fs.readFile(inputFileName, 'utf8', (err, data) => {
   if (err) throw err;
   const source = data.split('\n');
   const N = parseInt(source[0]);
   let i = N;
-  let keysAry = Array(N), keysMap = {};
+  let publics = [],
+    privates = [];
   while (i--) {
     let key = source[i + 1];
-    keysAry[i] = key;
-    keysMap[key] = false;
-  }
-  let totalPairs = 0;
-  while (keysAry.length > 1) {
-    let keyToTest = keysAry.pop();
-    for (let i = keysAry.length; i--;) {
-      let key = keysAry[i];
-      if (!isKeyPair(key, keyToTest)) {
-        continue;
-      }
-      keysAry.splice(i, 1);
-      keysMap[key] = true;
-      keysMap[keyToTest] = true;
-      ++totalPairs;
+    if (-1 === key.indexOf('a')) {
+      privates.push(key);
+    } else {
+      publics.push(key);
     }
+  }
+  privates.sort();
+  publics.sort();
+  let totalPairs = 0,
+    lpub = publics.length,
+    ipub = 0;
+  while (ipub < lpub && privates.length) {
+    let pub = publics[ipub],
+      ipriv = privates.length;
+    while (ipriv--) {
+      let priv = privates[ipriv];
+      if (isKeyPair(priv, pub)) {
+        ++totalPairs;
+        privates.splice(ipriv, 1);
+        break;
+      }
+    }
+    // move on...
+    ++ipub;
   }
   fs.writeFile(outputFileName, totalPairs, err => {
     if (err) throw err;
@@ -38,31 +50,24 @@ fs.readFile(inputFileName, 'utf8', (err, data) => {
   });
 });
 
-const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-const alphabetLength = alphabet.length;
-
 function isKeyPair(first, second) {
-  let result = true;
+  let result = true,
+    lastIndexFirst = first.length - 1,
+    lastIndexSecond = second.length - 1;
+  let occurrences = Array(alphabetLength);
   // let keyMap = {};
-  let seqLength = 0;
   for (let i = 0; i < alphabetLength; i++) {
     let char = alphabet[i],
-      nextChar = alphabet[i + 1],
       index1 = first.indexOf(char),
       index2 = second.indexOf(char),
       exists = -1 === index1 ? 0 : 1;
     exists += -1 === index2 ? 0 : 1;
     if (2 === exists) {
       // duplicate
-      seqLength = 0;
-      break;
+      return false;
     }
-    if (!exists && nextChar && (-1 === first.indexOf(nextChar) || -1 === second.indexOf(nextChar))) {
-      // either an end of a valid sequence or a gap
-      seqLength = first.indexOf(nextChar) === second.indexOf(nextChar) ? seqLength : 0;
-      break;
-    }
-    ++seqLength;
+    occurrences[i] = exists;
   }
-  return !!seqLength;
+  return -1 === occurrences.indexOf(0)
+    || occurrences.indexOf(0) > occurrences.lastIndexOf(1);
 }
