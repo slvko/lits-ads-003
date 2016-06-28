68ac30335c3e7fef1ff92584fa1fdeeb53d99af3
Yaroslav Dmytriv <yaroslav_dmytriv@epam.com>
2016-06-29 01:39:04 +0300

    simplify solutions array

--- ijones/ijones.js
+++ ijones/ijones.js
@@ -1,5 +1,5 @@
 'use strict';
-// console.time('Took: ');
+console.time('Took: ');
 const fs = require('fs'),
   BN = require('bn.js');
 
@@ -14,10 +14,7 @@ fs.readFile(inputFileName, 'utf8', (err, data) => {
   let contents = data.split('\n'),
     [w, h] = contents[0].split(' ').map(x => parseInt(x)),
     matrix = contents.slice(1, h+1),
-    solutions = {
-      prev: Array.apply(null, Array(h)).map(() => new BN(0)),
-      curr: Array.apply(null, Array(h)).map(() => new BN(0))
-    },
+    solutions = Array.apply(null, Array(h)).map(() => new BN(0)),
     jumps = new Map();
 
   /* base cases */
@@ -25,29 +22,29 @@ fs.readFile(inputFileName, 'utf8', (err, data) => {
     jumps.set(char, new BN(0))
   });
   for (let i = 0; i < h; i++) {
-    solutions.prev[i] = new BN((w > 1) ? 1 : 0);
+    solutions[i] = new BN((w > 1) ? 1 : 0);
     jumps.set(matrix[i][0], jumps.get(matrix[i][0]).add(new BN(1)));
   }
 
   /* filling the table */
   for (let i = 1; i < w - 1; i++) {
     for (let j = 0; j < h; j++) {
-      solutions.curr[j] = getSolutionForElement(j, i);
+      solutions[j] = getSolutionForElement(j, i);
     }
     for (let j = 0; j < h; j++) {
       let char = matrix[j][i];
       if (jumps.has(char)) {
-        jumps.set(char, jumps.get(char).add(solutions.curr[j]));
+        jumps.set(char, jumps.get(char).add(solutions[j]));
       }
     }
-    solutions.prev = solutions.curr.slice();
+    // solutions.prev = solutions.curr.slice();
   }
   let topRSolution = getSolutionForElement(h-1, w-1),
     bottomRSolution = getSolutionForElement(0, w-1);
   fs.writeFile(outputFileName, topRSolution.add(h > 1 ? bottomRSolution : new BN(0)), err => {
     if (err) throw err;
-    // console.log(topRSolution.add(h > 1 ? bottomRSolution : new BN(0)).toString());
-    // console.timeEnd('Took: ');
+    console.log(topRSolution.add(h > 1 ? bottomRSolution : new BN(0)).toString());
+    console.timeEnd('Took: ');
   });
 
   function getSolutionForElement(hIndex, wIndex) {
@@ -59,7 +56,7 @@ fs.readFile(inputFileName, 'utf8', (err, data) => {
     if (char === leftChar) {
       return jumps.get(char);
     }
-    return solutions.prev[hIndex].add(jumps.get(char));
+    return solutions[hIndex].add(jumps.get(char));
   }
 
 });
